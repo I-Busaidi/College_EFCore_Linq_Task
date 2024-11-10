b8a6d33e166834f3f6e184251eb6f02595a5fdd2
Ibrahim Al Busaidi <i.s.busaidi@gmail.com>
2024-11-10 16:51:33 +0400

    added a function to get all courses from the database along their enrolled students and faculty in the course repository.

--- Repositories/CourseRepository.cs
+++ Repositories/CourseRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using College_EFCore_Linq_Task.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,19 @@ namespace College_EFCore_Linq_Task.Repositories
 {
     public class CourseRepository
     {
+        private readonly ApplicationDbContext _context;
 
+        public CourseRepository (ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Course> GetAllCourses()
+        {
+            return _context.Courses.Include(c => c.StudentsInCourse)
+                                   .ThenInclude(s => s.Student)
+                                   .ThenInclude(f => f.Teacher)
+                                   .ToList();
+        }
     }
 }
