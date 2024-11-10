49ddc2d49c31f59265d915cec9abd252657dd108
Ibrahim Al Busaidi <i.s.busaidi@gmail.com>
2024-11-10 16:01:30 +0400

    added functions in subjects repository.

--- Repositories/SubjectRepository.cs
+++ Repositories/SubjectRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using College_EFCore_Linq_Task.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,54 @@ namespace College_EFCore_Linq_Task.Repositories
 {
     public class SubjectRepository
     {
+        private readonly ApplicationDbContext _context;
+        public SubjectRepository (ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
+        public IEnumerable<Subject> GetAllSubjects()
+        {
+            return _context.Subjects.Include(s => s.Teacher);
+        }
+
+        public Subject GetSubjectById(int subjectId)
+        {
+            return _context.Subjects.Where(s => s.Subject_Id == subjectId)
+                                    .Include(s => s.Teacher)
+                                    .SingleOrDefault();
+        }
+
+        public void AddSubject(Subject subject)
+        {
+            _context.Subjects.Add(subject);
+            _context.SaveChanges();
+        }
+
+        public void UpdateSubject(Subject subject)
+        {
+            _context.Subjects.Update(subject);
+            _context.SaveChanges();
+        }
+
+        public void DeleteSubject(int Subject_Id)
+        {
+            var subject = GetSubjectById(Subject_Id);
+            if (subject != null)
+            {
+                _context.Subjects.Remove(subject);
+                _context.SaveChanges();
+            }
+        }
+
+        public IEnumerable<Subject> GetSubjectsTaughtByFaculty(int FID)
+        {
+            return _context.Subjects.Where(s => s.Teacher_Id == FID).ToList();
+        }
+
+        public int CountSubjects()
+        {
+            return _context.Subjects.Count();
+        }
     }
 }
